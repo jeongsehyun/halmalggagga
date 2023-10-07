8c28b952f59005815ae84a487560951bcf2f0b39
jeongsehyun <sehyun.dev@gmail.com>
2023-10-07 23:25:37 +0900

    화면 마크업

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -2,11 +2,15 @@ import "./index.scss";
 import { FaX, FaO } from "react-icons/fa6";
 import BorderBox from "../BorderBox";
 
-function Comment({ type, comment }) {
+function Comment({ type, comment, userInfo }) {
+  const { userName } = userInfo;
   return (
     <div className="pointer commentbox-container">
-      <div className="img-box">
-        <div className="img"></div>
+      <div className="profile">
+        <div className="img-box">
+          <div className="img"></div>
+        </div>
+        <p className="user-name">{userName}</p>
       </div>
       <BorderBox boxClass="comment-box">
         {type === "O" ? <FaO className="icon-type type-o" /> : <FaX className="icon-type type-x" />}
--- src/components/VotingStatus/index.jsx
+++ src/components/VotingStatus/index.jsx
@@ -1,5 +1,4 @@
 import BarChart from "../BarChart";
-import Comment from "../Comment";
 import "./index.scss";
 import { FaX, FaO } from "react-icons/fa6";
 
@@ -13,7 +12,6 @@ function VotingStatus() {
         "<span>열심히 공부한다</span>"
       </p>
       <BarChart />
-      <Comment type="O" comment="아자 아자 화이팅!" />
     </div>
   );
 }
